2657fe65a85a13cc17ae742f28d5bdbdfc257e99
Alex Barrón <abarroncor@gmail.com>
2022-05-11 12:17:10 -0500

    Add python example

---
+++ hello.py
@@ -0,0 +1,12 @@
+"""
+Simple test program in Python
+Alex Barrón
+"""
+
+def main():
+    print ("Hello world")
+    print ("No sé que pasó")
+    for i in range(5):
+        print ("AWANTAAAA")
+
+main()
