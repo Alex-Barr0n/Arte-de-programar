6c18eb1454a84f18870c3bdcfc539a5c4ed1a664
Israel Macías <israel_macias@Israels-MacBook-Pro.local>
2022-05-11 12:40:50 -0500

    edit de isra

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 """
 Simple test program in Python
 Alex Barrón
+Isra Macías
 """
 
 def main():
+    print ("soy isra")
     print ("Hello world")
     print ("No sé que pasó")
     for i in range(5):
